7ceb576a1264b15f269f6747a8b713450adfdef6
Ninad Page <npage@schubergphilis.com>
2022-07-20 20:59:26 +0200

    feat: typer instead of click, logging, version
    - Replace click with Typer for CLI
    - Add logging using coloredlogs
    - Add CLI command to print version and exit

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,1 +1,13 @@
 """{{cookiecutter.friendly_name}}."""
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
+
+
+# This is a fallback for using `importlib.metadata`.
+# DO NOT modify the version manually - it is updated by python-semantic-release
+__static_version = "0.0.0"
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = __static_version
--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,12 +1,55 @@
 """Command-line interface."""
-import click
+import logging
+import os
+from typing import Optional
+
+import coloredlogs
+import typer
+
+from {{cookiecutter.package_name}} import __version__
+
+logger = logging.getLogger("{{cookiecutter.package_name}}")
+app = typer.Typer()
 
 
-@click.command()
-@click.version_option()
+def version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{{cookiecutter.project_name}}: {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def cli(
+    version: Optional[bool] = typer.Option(  # noqa: B008, F841
+        None, "--version", callback=version_callback, is_eager=True
+    ),
+    log_level: str = "INFO",
+) -> None:
+    """Command-line interface."""
+    coloredlogs_args = {}
+    if os.getenv("CI"):
+        # force tty for colors when running in CI
+        coloredlogs_args["isatty"] = True
+    level = coloredlogs.level_to_number(log_level.upper())
+    if level > logging.DEBUG:
+        coloredlogs.install(level=level, **coloredlogs_args)
+    else:
+        # Only set DEBUG level for the CLI logger and keep other loggers at INFO
+        coloredlogs.install(level="INFO", **coloredlogs_args)
+        logger.propagate = False
+        coloredlogs.install(logger=logger, level=level, **coloredlogs_args)
+
+
+# Sample typer subcommand 'hello'
+# @app.command()
+# def hello() -> None:
+#     """sample command hello"""
+#     logging.info("Hello")
+
+
 def main() -> None:
-    """{{cookiecutter.friendly_name}}."""
+    app()
 
 
 if __name__ == "__main__":
-    main(prog_name="{{cookiecutter.project_name}}")  # pragma: no cover
+    app()
